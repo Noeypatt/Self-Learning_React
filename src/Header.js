98a4971a0ae01cb4d350eb9700ac33934aa1474e
pat <patnarin241240@gmail.com>
2020-04-09 04:38:41 +0700

    :art:header

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import 'moment/locale/th';
-import { Jumbotron, Button, Input } from 'reactstrap';
-import Cloudy from './image/cloudy.svg'
+import { Jumbotron, Button, Input, Card, CardTitle, CardText } from 'reactstrap';
+import Cloudy from './image/cloudy.svg';
+import Plane from './image/plane.svg';
+import Car from './image/car.svg';
+import Bus from './image/Bus.svg';
+import Cruise from './image/cruise.svg';
 
 moment.locale('th');
 console.log(moment.locale());
@@ -10,12 +14,31 @@ console.log(moment.locale());
 const Header = () => {
 
     const date = moment().add(543, 'year').format('LL')
+    const dataCard = [
+        {
+            title: "Title",
+            icon: Plane
+        },
+        {
+            title: "Title",
+            icon: Bus 
+        },
+    ]
+    const dataCard2 = [
+        {
+            title: "Title",
+            icon: Car
+        },
+        {
+            title: "Title",
+            icon: Cruise
+        },
+    ]
 
     return (
         <div className="warp-header">
             <div className="header-left-content">
                 <Jumbotron>
-                    {/* <h1 className="display-3"><img src={Cloudy} alt="planet-Cloudy" />Hello, world!</h1> */}
                     <div className="header-title">
                         <img src={Cloudy} alt="planet-earth" />
                         <h6>วันที่ {date} </h6>
@@ -24,13 +47,35 @@ const Header = () => {
                 </Jumbotron>
                 <div className="header-search">
                     <Input type="text" name="search" id="search" placeholder="Search" />
-                    <Button color="secondary">Search</Button>
+                    <Button color="info">Search</Button>
                 </div>
             </div>
             <div className="header-right-content">
-
+                <div className="header-card">
+                    {
+                        dataCard.map((item, index) => {
+                            return (
+                                <Card key={index}>
+                                    <img src={item.icon} />
+                                    <CardTitle>{item.title}</CardTitle>
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
+                <div className="header-card">
+                    {
+                        dataCard2.map((item, index) => {
+                            return (
+                                <Card key={index}>
+                                    <img src={item.icon} />
+                                    <CardTitle>{item.title}</CardTitle>
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
             </div>
-
         </div>
     )
 }
