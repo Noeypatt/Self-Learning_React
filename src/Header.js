1e42a66ff2ea60f03687b8b547996341be5b9ee6
pat <patnarin241240@gmail.com>
2020-04-09 03:09:04 +0700

    :art:header monentjs

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import moment from 'moment';
+import 'moment/locale/th';
 import { Jumbotron, Button, Input } from 'reactstrap';
+import Cloudy from './image/cloudy.svg'
+
+moment.locale('th');
+console.log(moment.locale());
 
 const Header = () => {
+
+    const date = moment().add(543, 'year').format('LL')
+
     return (
         <div className="warp-header">
             <div className="header-left-content">
                 <Jumbotron>
-                    <h1 className="display-3">Hello, world!</h1>
-                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                    <hr className="my-2" />
-                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <p className="lead">
-                        <Button color="primary">Learn More</Button>
-                    </p>
+                    {/* <h1 className="display-3"><img src={Cloudy} alt="planet-Cloudy" />Hello, world!</h1> */}
+                    <div className="header-title">
+                        <img src={Cloudy} alt="planet-earth" />
+                        <h6>วันที่ {date} </h6>
+                    </div>
+                    <p></p>
                 </Jumbotron>
                 <div className="header-search">
                     <Input type="text" name="search" id="search" placeholder="Search" />
-                    <Button>Search</Button>
+                    <Button color="secondary">Search</Button>
                 </div>
             </div>
             <div className="header-right-content">
