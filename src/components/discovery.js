2dd20ebec0ee6d31d5441f62dfd848acbfd798ea
pat <patnarin241240@gmail.com>
2020-04-11 04:20:23 +0700

    :lipstick:Discovery

--- src/components/discovery.js
+++ src/components/discovery.js
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { setLike } from '../redux/discovery/action'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
 import { Card, CardImg, CardText, CardBody, CardFooter, } from 'reactstrap';
 import Dis from '../image/dis1.jpg'
 import Dis2 from '../image/dis2.jpg'
@@ -10,9 +13,6 @@ import Dis3 from '../image/dis3.jpg'
 import Dis4 from '../image/dis4.jpg'
 
 const Discovery = () => {
-
-
-
 
     const like = useSelector(state => state.like)
     const action = bindActionCreators({ ...setLike }, useDispatch())
@@ -51,7 +51,11 @@ const Discovery = () => {
 
     return (
         <div className="warp-discovery">
-            <h5>Discovery</h5>
+            <div className="discovery-title">
+                <h5>Discovery</h5>
+                <p>View all<FontAwesomeIcon icon={faAngleRight} /></p>
+            </div>
+
             <div className="discovery-content">
                 {
                     data.map((item, index) => {
