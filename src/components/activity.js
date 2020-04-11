d7e488f9e020ac5f68c8a85ea8c9bfa747cc4b28
pat <patnarin241240@gmail.com>
2020-04-11 21:42:14 +0700

    :lipstick:activity

--- src/components/activity.js
+++ src/components/activity.js
@@ -1,20 +1,14 @@
 import React from 'react'
-import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
-import Act from '../image/act1.jpg';
-import Act2 from '../image/act2.jpg';
-
 
 const Activitiy = () => {
 
     const data = [
         {
-            img: Act,
             title: "Title",
             text: "Description",
             small: "smail text"
         },
         {
-            img: Act2,
             title: "Title",
             text: "Description",
             small: "smail text"
@@ -30,16 +24,7 @@ const Activitiy = () => {
                 {
                     data.map((item, index) => {
                         return (
-                            <Card>
-                                <CardImg top width="100%" src={item.img} alt={`img-${index}`} />
-                                <CardBody>
-                                    <CardTitle>{item.title}</CardTitle>
-                                    <CardText>{item.text}</CardText>
-                                    <CardText>
-                                        <small className="text-muted">{item.small}</small>
-                                    </CardText>
-                                </CardBody>
-                            </Card>
+                            <div></div>
                         )
                     })
                 }
