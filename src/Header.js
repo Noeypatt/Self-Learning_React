d8be0abf7f79e487427e7b253cead268e5af4704
pat <patnarin241240@gmail.com>
2020-04-13 01:43:17 +0700

    :tada:responsive

--- src/Header.js
+++ src/Header.js
@@ -11,6 +11,7 @@ import Cloudy from './image/cloudy.svg';
 const Header = () => {
 
     const date = moment().format('LL')
+    const isIpad = useMediaQuery({ maxWidth: 1024 })
     const isIpadMini = useMediaQuery({ maxWidth: 768 })
     const isMobile = useMediaQuery({ maxWidth: 576 })
 
@@ -97,45 +98,86 @@ const Header = () => {
                             </div>
                         </div>
                         :
-                        <div className="warp-header">
-                            <div className="header-left-content">
-                                <Jumbotron>
-                                    <div className="header-title">
-                                        <div className="header-title-date">
-                                            <img src={Cloudy} alt="planet-earth" />
-                                            <h6> {date} </h6>
+                        isIpad ?
+                            <div className="warp-header">
+                                <div className="header-left-content">
+                                    <Jumbotron>
+                                        <div className="header-title">
+                                            <div className="header-title-date">
+                                                <img src={Cloudy} alt="planet-earth" />
+                                                <h6> {date} </h6>
+                                            </div>
+                                            <div className="header-title-content">
+                                                <h3>Have you ever seen the orange clouds?</h3>
+                                                <p>orange clouds | blue sky</p>
+                                            </div>
+                                            <div className="header-title-btn">
+                                                <Button>Get start</Button>
+                                            </div>
                                         </div>
-                                        <div className="header-title-content">
-                                            <h3>Have you ever seen the orange clouds?</h3>
-                                            <p>orange clouds | blue sky</p>
-                                        </div>
-                                        <div className="header-title-btn">
-                                            <Button>Get start</Button>
-                                        </div>
+                                    </Jumbotron>
+                                    <div className="header-search">
+                                        <Input type="text" name="search" id="search" placeholder="Search" />
                                     </div>
-                                </Jumbotron>
-                                <div className="header-search">
-                                    <Input type="text" name="search" id="search" placeholder="Search" />
+                                </div>
+                                <div className="header-right-content">
+                                    {
+                                        dataList.map((item, index) => {
+                                            return (
+                                                <ListGroup key={index} >
+                                                    <ListGroupItem>
+                                                        <ListGroupItemHeading>{item.title}</ListGroupItemHeading>
+                                                        <ListGroupItemText>
+                                                            <p>{item.description}</p>
+                                                        </ListGroupItemText>
+                                                    </ListGroupItem>
+                                                </ListGroup>
+                                            )
+                                        })
+                                    }
+
                                 </div>
                             </div>
-                            <div className="header-right-content">
-                                {
-                                    dataList.map((item, index) => {
-                                        return (
-                                            <ListGroup key={index} className={`animated fadeInRight delay-${item.delay}s`}>
-                                                <ListGroupItem>
-                                                    <ListGroupItemHeading>{item.title}</ListGroupItemHeading>
-                                                    <ListGroupItemText>
-                                                        <p>{item.description}</p>
-                                                    </ListGroupItemText>
-                                                </ListGroupItem>
-                                            </ListGroup>
-                                        )
-                                    })
-                                }
+                            :
+                            <div className="warp-header">
+                                <div className="header-left-content">
+                                    <Jumbotron>
+                                        <div className="header-title">
+                                            <div className="header-title-date">
+                                                <img src={Cloudy} alt="planet-earth" />
+                                                <h6> {date} </h6>
+                                            </div>
+                                            <div className="header-title-content">
+                                                <h3>Have you ever seen the orange clouds?</h3>
+                                                <p>orange clouds | blue sky</p>
+                                            </div>
+                                            <div className="header-title-btn">
+                                                <Button>Get start</Button>
+                                            </div>
+                                        </div>
+                                    </Jumbotron>
+                                    <div className="header-search">
+                                        <Input type="text" name="search" id="search" placeholder="Search" />
+                                    </div>
+                                </div>
+                                <div className="header-right-content">
+                                    {
+                                        dataList.map((item, index) => {
+                                            return (
+                                                <ListGroup key={index} className={`animated fadeInRight delay-${item.delay}s`}>
+                                                    <ListGroupItem>
+                                                        <ListGroupItemHeading>{item.title}</ListGroupItemHeading>
+                                                        <ListGroupItemText>
+                                                            <p>{item.description}</p>
+                                                        </ListGroupItemText>
+                                                    </ListGroupItem>
+                                                </ListGroup>
+                                            )
+                                        })
+                                    }
 
+                                </div>
                             </div>
-                        </div>
             }
         </React.Fragment>
 
