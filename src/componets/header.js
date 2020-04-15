ee419fc72a96f6022e0596efedbc889253cbc87b
pat <patnarin241240@gmail.com>
2020-04-16 06:36:48 +0700

    :lipstic:Header

--- src/componets/header.js
+++ src/componets/header.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'antd';
 
 const Header = () => {
     return (
@@ -7,7 +8,16 @@ const Header = () => {
                 <h1>ศูนย์รับเลี้ยง สุนัข และแมวจรจัด</h1>
                 <h2>สัตว์เลี้ยงทุกตัว ล้วนต้องการความรัก เเละการดูเเล</h2>
                 <h2>ให้คุณได้เป็นส่วนหนึ่ง ที่ได้ช่วยเหลือพวกเขา</h2>
+                <Button>
+                    <h3>มองหาเพื่อนรู้ใจ</h3>
+                </Button>
             </div>
+
+            <div className="header-photo">
+                <p>Photo by Lum3n from Pexels</p>
+            </div>
+
+
 
         </div>
     )
