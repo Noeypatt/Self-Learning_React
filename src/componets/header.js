bf442395072e2385f47d7c5bc38eedefa1c46f9e
pat <patnarin241240@gmail.com>
2020-04-16 05:48:33 +0700

    :sparkles:google fonts

--- src/componets/header.js
+++ src/componets/header.js
@@ -3,6 +3,10 @@ import React from 'react'
 const Header = () => {
     return (
         <div className="warp-header">
+            <div className="header-container">
+                <h1>Be kind to dog</h1>
+                <h2>Every dog ​​deserves love and care.</h2>
+            </div>
 
         </div>
     )
